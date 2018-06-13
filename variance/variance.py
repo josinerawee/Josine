dd358b2563c22942114d0c2834847a5b4438133d
Josine Rawee <josinerawee@MBP-van-Josine.fritz.box>
2018-06-14 00:55:42 +0200

    added distribution of vizualization per category

--- variance/variance.py
+++ variance/variance.py
@@ -8,6 +8,36 @@ from collections import Counter
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
+from webcolors import name_to_rgb
+import math
+from PIL import Image,ImageDraw
+
+from PIL import Image, ImageDraw
+import colorsys
+
+def make_rainbow_rgb(colors, width, height):
+    """colors is an array of RGB tuples, with values between 0 and 255"""
+
+    img = Image.new("RGBA", (width, height))
+    canvas = ImageDraw.Draw(img)
+
+    def hsl(x):
+        to_float = lambda x : x / 255.0
+        (r, g, b) = map(to_float, x)
+        h, s, l = colorsys.rgb_to_hsv(r,g,b)
+        h = h if 0 < h else 1 # 0 -> 1
+        return h, s, l
+
+    rainbow = sorted(colors, key=hsl)
+
+    dx = width / float(len(colors)) 
+    x = 0
+    y = height / 2.0
+    for rgb in rainbow:
+        canvas.line((x, y, x + dx, y), width=height, fill=rgb)
+        x += dx
+    #img.show() 
+    return img
 
 def makecounter(listallhypernym,input):
 	for i in input:
@@ -46,10 +76,13 @@ def main():
 	if len(sys.argv) == 2:
 		space=sys.argv[1]
 	if space=='fullcolors':
-		f=open('fullcolorsdm.txt','r')
+	 	f=open('fullcolorsdm.txt','r')
+	 	colors=['green', 'blue', 'purple', 'pink', 'brown', 'red', 'yellow', 'orange', 'grey', 'teal', 'gray', 'violet', 'magenta', 'turquoise', 'rose', 'lavender', 'tan', 'olive', 'cyan', 'aqua', 'white', 'lime', 'beige', 'sienna', 'lilac', 'mauve', 'salmon', 'maroon', 'gold', 'peach', 'foam', 'drab', 'color', 'mustard', 'indigo', 'black', 'periwinkle', 'fuschia', 'sky', 'umber', 'plum', 'navy', 'mint']
 	if space=='reducedcolors':
-		f=open('reducedcolorsdm.txt','r')
+	 	f=open('reducedcolorsdm.txt','r')
+	 	colors=['green', 'blue', 'purple', 'pink', 'brown', 'red', 'yellow', 'grey', 'teal', 'gray', 'violet', 'magenta', 'turquoise', 'cyan', 'white', 'beige', 'lilac', 'mauve', 'indigo', 'black']
 
+	featuredict={}
 	variancedict={}
 	for line in f: 
 		featurelist=[]
@@ -59,7 +92,7 @@ def main():
 		features=features.split(' ')
 		for i in features: 
 			featurelist.append(float(i))
-		
+		featuredict[target]=featurelist
 		variance=np.var(featurelist)
 		variancedict[target]=variance
 	sortedvariance= sorted(variancedict.items(), key=operator.itemgetter(1))
@@ -93,8 +126,91 @@ def main():
 	# plt.grid(True)
 	# plt.show()
 
+	
 
 
+
+	#for each of these categories find the words that were in there + their features
+	hypernymdict={}
+	for x in categoriesonlytop:
+		hypernymdict[x]=[]
+		for i in top500:
+			target=i[0]
+			try:
+				synset=wn.synsets(target)
+			except UnicodeDecodeError:
+				pass
+			if synset==[]:
+				pass
+			else:	
+				hypernymslist=hypernym(synset[0])
+				hypernyms=[]
+				for h in hypernymslist:
+					for y in h:
+						y=y.lemma_names()
+						hypernyms.append(y)
+						hypernyms=hypernyms=flatten(hypernyms)
+				if x in hypernyms:
+					hypernymdict[x].append(target)
+	
+	finaldictcolors={}
+
+	
+	for key, value in hypernymdict.iteritems():
+		colordict={}
+		vectors=[]
+		for i in value:
+			vectors.append(featuredict[i])
+		mean=np.mean(np.array(vectors),axis=0)
+		summean=sum(mean)
+		count=0
+		for c in colors: 
+			if mean[count]==0:
+				colordict[c]=0
+				count+=1
+			else:
+				colordict[c]=int((mean[count]/summean)*100)
+				count+=1
+
+		finaldictcolors[key]=colordict
+
+
+	for key,value in finaldictcolors.iteritems():
+		pixels=[]
+		name=key
+		for key,value in finaldictcolors[key].iteritems():
+			for i in range(value):
+				if key =='color':
+					pass
+				elif key =='drab':
+					pixels.append((50, 113, 23))
+				elif key =='mint':
+					pixels.append((152,255,152))
+				elif key =='umber':
+					pixels.append((	99, 81, 71))
+				elif key =='sky':
+					pixels.append((135,206,250))
+				elif key =='rose':
+					pixels.append(((255, 0, 128)))
+				elif key =='fuschia':
+					pixels.append((202, 44, 146))
+				elif key =='peach':
+					pixels.append((255,218,185))
+				elif key=='mustard':
+					pixels.append((255, 219, 88))
+				elif key=='lilac':
+					pixels.append((200,162,200))
+				elif key =='mauve':
+					pixels.append((224, 176, 255))
+				elif key =='periwinkle':
+					pixels.append((195,205,230))
+				else:
+					pixels.append(name_to_rgb(key))
+
+		size=int(math.sqrt(len(pixels)))+1
+		width=size*15
+		image=make_rainbow_rgb(pixels, width, size)
+		image.save(space+'viz/'+name+'.png')
 
 
 
